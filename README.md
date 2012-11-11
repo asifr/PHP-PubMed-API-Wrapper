890ba95c23965342eea79bb3764cf08bece87eec
Asif Rahman <Asif@Asif.local>
2012-11-10 22:38:00 -0500

    Update readmne and documentation

--- README.md
+++ README.md
@@ -2,18 +2,53 @@ PHP PubMed API Wrapper
 ======================
 
 **Author:** Asif Rahman  
-**Website:** [Demo, Documentation, and License](http://neuralengr.com/AR_PubMedAPI/)
+**Demo:** [Example](http://neuralengr.com/asifr/PubMedAPI/)
 
 ## Description
 
-A self-contained PHP class provides a wrapper around the PubMed API. Fully documented with working examples.
+A standalone PHP class provides a wrapper around the PubMed API. Fully documented with working examples.
 
-- Use a proxy server while querying
-- Limit total number of search results
-- Pagination splits search results among multiple pages
+- Access PubMed through a proxy server programatically
+- Limit the total number of search results
+- Pagination splits search results amoung multiple pages
 - Optional exact search term matching
 - Cache search results as JSON formatted text files
 - Load search results from cache
-- Toggle lifetime of cache results
+- Control lifetime of cache results
 
 See the script in action at the [Soterix Medical](http://soterixmedical.com/learn/publications.php) publications browser.
+
+## Documentation
+
+To get started:
+
+    include('PubMedAPI.php');
+    $search_term = 'search query';
+    $PubMedAPI = new PubMedAPI();
+    $results = $PubMedAPI->query($search_term);
+
+The `query()` method returns an array of search results or an empty array if no matches were found.
+
+Interaction with the PubMed API is accomplished in two steps. Given a search term, the first query returns a list of PMIDs. The second query returns the metadata of each article. `query()` takes three arguments, the first is the search term string (this can also be the PMID if already known), the second is a boolean that returns a compact result if `TRUE`, and the third is an optional callback function to manipulate the results.
+
+Returns the pmid, volume, issue, year, month, pages, issn, journal, journalabbrev, title, abstract, affiliation, authors, articleid, keywords of each article.
+
+Refer to the class file for more documentation.
+
+## Copyright and License
+
+This script is free software, available under the terms of the BSD-style open source license reproduced below, or, at your option, under the [GNU General Public License version 2](http://www.gnu.org/licenses/gpl-2.0.txt) or a later version.
+
+PHP PubMed API Wrapper  
+Copyright © 2012 Asif Rahman  
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+Neither the name "PHP PubMedAPI Wrapper" nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright owner or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
--- index.php
+++ index.php
@@ -4,43 +4,27 @@
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	<title>PHP PubMed API Wrapper by Asif Rahman</title>
 	<style type="text/css" media="screen">
-		body{font-family:"Helvetica Neue",Helvetica,sans-serif;font-size:14px;}
+		body{font-family:"Helvetica Neue",Helvetica,sans-serif;font-size:14px;line-height:19px;}
+		h1 a{color:#A00;}h1 a:hover{text-decoration:none;}
 		pre,code{font-family:Monaco,monospace;font-size:12px;}pre{padding:20px;background:#EEE;}
-		a{color:#0080FF;text-decoration:none;}a:hover{color:#FF0080;}
-		table{width:800px;}
-		th{text-align:left;}
-		td{vertical-align:top;}
+		a{color:#00a;text-decoration:none;}a:hover{color:#000;text-decoration:underline;}
+		table{width:100%;}
+		th{text-align:left;border-bottom:1px solid #CCC;padding:5px;}
+		td{vertical-align:top;padding:5px;}
 		#wrap{width:960px;margin:0 auto;}
+		input[type=text]{width:300px;font-size:inherit;font-family:inherit;border:1px solid #BBB;padding:5px;}
 	</style>
 </head>
 <body>
 
-<a href="https://github.com/asifr/PHP-PubMed-API-Wrapper"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
-
 <div id="wrap">
-<h1>PHP PubMed API Wrapper</h1>
-<p><strong>Author</strong>: Asif Rahman</p>
+<h1><a href="https://github.com/asifr/PHP-PubMed-API-Wrapper">PHP PubMed API Wrapper</a></h1>
+<p><strong>Author</strong>: <a href="https://github.com/asifr">Asif Rahman</a></p>
 
-<p><a href="https://github.com/asifr/PHP-PubMed-API-Wrapper">Download</a></p>
-
-<h2>Description</h2>
-
-<p>The PubMed API Wrapper provides a convenient method to query the PubMed database.</p>
-<ul>
-	<li>Use a proxy server while querying</li>
-	<li>Limit total number of search results</li>
-	<li>Pagination splits search results among multiple pages</li>
-	<li>Optional exact search term matching</li>
-	<li>Cache search results as JSON formatted text files</li>
-	<li>Load search results from cache</li>
-	<li>Toggle lifetime of cache results</li>
-</ul>
-<p>See the script in action at <a href="http://soterixmedical.com/learn/publications.php">Soterix Medical</a>.</p>
-
-<h2>Demo</h2>
+<p>This demo is minimal in scope. It uses the PubMedAPI class to query PubMed and return the results in a table.</p>
 
 <form action="" method="get" accept-charset="utf-8">
-	<p><label for="search_term">Search term: </label> <input type="text" name="term" value="" id="search_term"> <input type="submit" value="Search PubMed"></p>
+	<p><label for="search_term">Search term: </label> <input type="text" name="term" value="<?php echo isset($_GET['term'])?stripslashes($_GET['term']):'electrical stimulation network dynamics reato 2010'; ?>" id="search_term"> <input type="submit" value="Search PubMed"></p>
 </form>
 
 <?php
@@ -69,18 +53,18 @@ if (isset($_GET['term']) && $_GET['term'] != '') {
 
 	remove_bad_characters();
 
-	include('AR_PubMedAPI.php');
-	$AR_PubMedAPI = new AR_PubMedAPI();
+	include('PubMedAPI.php');
+	$PubMedAPI = new AR_PubMedAPI();
 	if (isset($_GET['page'])) {
-		$AR_PubMedAPI->retstart = $AR_PubMedAPI->retmax*((int)$_GET['page'] - 1)+1;
+		$PubMedAPI->retstart = $PubMedAPI->retmax*((int)$_GET['page'] - 1)+1;
 	}
-	$results = $AR_PubMedAPI->query($term, false);
+	$results = $PubMedAPI->query($term, false);
 }
 ?>
 
 <?php if (!empty($results)): ?>
-	<p>Search results for <strong><?php echo urldecode($AR_PubMedAPI->term); ?></strong> (<?php echo $AR_PubMedAPI->count; ?> results, showing max 5)</p>
-	<table border="0" cellspacing="5" cellpadding="5">
+	<p>Search results for <strong><?php echo urldecode($PubMedAPI->term); ?></strong> (<?php echo $PubMedAPI->count; ?> results, showing max 5)</p>
+	<table border="0" cellspacing="0" cellpadding="0">
 		<tr>
 			<th>PMID</th>
 			<th>Title</th>
@@ -100,28 +84,13 @@ if (isset($_GET['term']) && $_GET['term'] != '') {
 	</table>
 <?php endif; ?>
 
-<h2>Documentation</h2>
-
-<p><strong>Example implementation:</strong></p>
-<pre><code>include('AR_PubMedAPI.php');
+<h2><strong>Example implementation:</strong></h2>
+<pre><code>include('PubMedAPI.php');
 $search_term = 'electrical stimulation network dynamics reato 2010';
-$AR_PubMedAPI = new AR_PubMedAPI();
-$results = $AR_PubMedAPI->query($search_term);
+$PubMedAPI = new PubMedAPI();
+$results = $PubMedAPI->query($search_term);
 </code></pre>
 
-<h2>Copyright and License</h2>
-
-<p>The PHP script is free software, available under the terms of the BSD-style open source license reproduced below, or, at your option, under the <a href="http://www.gnu.org/licenses/gpl-2.0.txt">GNU General Public License version 2</a> or a later version.</p>
-
-<p>PHP PubMed API Wrapper<br />
-Copyright &copy; 2012 Asif Rahman<br />
-All rights reserved.</p>
-
-<p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>
-<p>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</p>
-<p>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</p>
-<p>Neither the name "PHP PubMedAPI Wrapper" nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.</p>
-<p>This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright owner or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.</p>
 </div>
 
 </body>
