5180301d7b69fd3a5f00081c2fb706b916853c63
Asif Rahman <Asif@Asif.local>
2012-11-10 19:06:01 -0500

    Minor changes

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 
-<a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
+<a href="https://github.com/asifr/PHP-PubMed-API-Wrapper"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
 
 <div id="wrap">
 <h1>PHP PubMed API Wrapper</h1>
